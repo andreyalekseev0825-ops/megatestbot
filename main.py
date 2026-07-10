01bd375748c273d110d12b9aa9edce0a2e8f7831
dmitrybarinov06625 <dmitrybarinov06625@gmail.com>
2026-07-10 13:35:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -244,8 +244,8 @@ def reminder_loop():
             
             # Время для напоминаний: 17:30, 18:30, 19:30 (МСК)
             reminder_times = [
-                {"hour": 17, "minute": 00, "start_remind": 8, "start_minute": 20},
-                {"hour": 17, "minute": 00, "start_remind": 8, "start_minute": 25},
+                {"hour": 14, "minute": 00, "start_remind": 8, "start_minute": 40},
+                {"hour": 15, "minute": 00, "start_remind": 9, "start_minute": 5},
                 {"hour": 15, "minute": 30, "start_remind": 15, "start_minute": 5},
                 {"hour": 16, "minute": 30, "start_remind": 16, "start_minute": 5}
             ]
