cbac77a80aa39d5c37dc56607cb1d35dad61df36
dmitrybarinov06625 <dmitrybarinov06625@gmail.com>
2026-07-12 01:07:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -248,7 +248,7 @@ def reminder_loop():
             
             # --- НАПОМИНАЛКИ (ПО ТВОЕМУ ВРЕМЕНИ UTC+2) ---
             reminder_times = [
-                {"hour": 21, "minute": 30, "start_remind": 1, "start_minute": 5},  # 17:30 по твоему времени
+                {"hour": 21, "minute": 30, "start_remind": 23, "start_minute": 10},  # 17:30 по твоему времени
                 {"hour": 22, "minute": 30, "start_remind": 23, "start_minute": 15},  # 18:30 по твоему времени
                 # 19:30 по твоему времени
             ]
